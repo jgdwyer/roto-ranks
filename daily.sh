5e462e2ae2fd0f220e92dd1fd698d3cf8ed7e925
John Dwyer <jgdwyer@gmail.com>
2017-06-22 17:41:08 +0000

    Roto file csv output now in csv folder

--- daily.sh
+++ daily.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/ubuntu/.bash_aliases && source /home/ubuntu/anaconda3/bin/activate /home/ubuntu/anaconda3/envs/roto && python /home/ubuntu/roto-ranks/roto_ranks.py
-aws s3 cp index.html s3://jgdwyer.com/roto-ranks/
-aws s3 cp roto_ranks.csv s3://jgdwyer.com/roto-ranks/data/
+aws s3 cp /home/ubuntu/roto-ranks/index/index.html s3://jgdwyer.com/roto-ranks/
+aws s3 cp /home/ubuntu/roto-ranks/csv/roto_ranks.csv s3://jgdwyer.com/roto-ranks/data/
 

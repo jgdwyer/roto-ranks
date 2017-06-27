be587065819710bd8c0039340ab1678b2e36dcc2
John Dwyer <jgdwyer@gmail.com>
2017-06-27 21:44:40 +0000

    Transfer image files and stats csv

--- daily.sh
+++ daily.sh
@@ -2,4 +2,6 @@
 source /home/ubuntu/.bash_aliases && source /home/ubuntu/anaconda3/bin/activate /home/ubuntu/anaconda3/envs/roto && python /home/ubuntu/roto-ranks/roto_ranks.py
 aws s3 cp /home/ubuntu/roto-ranks/index/index.html s3://jgdwyer.com/roto-ranks/
 aws s3 cp /home/ubuntu/roto-ranks/csv/roto_ranks.csv s3://jgdwyer.com/roto-ranks/data/
-
+aws s3 cp /home/ubuntu/roto-ranks/csv/roto_stats.csv s3://jgdwyer.com/roto-ranks/data/
+aws s3 cp /home/ubuntu/roto-ranks/figs/roto_ranks_bar_chart.png s3://jgdwyer.com/roto-ranks/figs/
+aws s3 cp /home/ubuntu/roto-ranks/figs/timeseries.png s3://jgdwyer.com/roto-ranks/figs/
